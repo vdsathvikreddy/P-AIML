2c68a0894d3912ffe424c441c83a44c536c79c32
vdsathvikreddy <163766078+vdsathvikreddy@users.noreply.github.com>
2025-04-25 22:38:23 +0530

    Add files via upload
    ADDED FILES

---
+++ basic_streamlit.py
@@ -0,0 +1,20 @@
+import streamlit as st
+from PIL import Image
+img = Image.open("code_sst_2.png")
+import sst_2_logistic_regression
+
+
+st.title("Recursive Deep Models for Semantic Compositionality Over a Sentiment Treebank")
+
+st.header("Training our model using SVM")
+
+if st.button("classification report using SVM"):
+    st.image(img, width = 2000)
+
+name = st.text_input("Enter ur review about bahubali", "Type Here ...")
+
+# display the name when the submit button is clicked
+# .title() is used to get the input text string
+if(st.button('Submit')):
+    out = sst_2_logistic_regression.predict_sentiment(name)
+    st.text(out)
