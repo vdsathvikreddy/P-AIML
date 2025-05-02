4781a750f102b34b483f4fdd307cefe432dbc632
vdsathvikreddy <163766078+vdsathvikreddy@users.noreply.github.com>
2025-05-02 18:19:02 +0530

    Update basic_streamlit.py

--- basic_streamlit.py
+++ basic_streamlit.py
@@ -1,7 +1,7 @@
 import streamlit as st
 from PIL import Image
-# import sst_2_logistic_regression
-import sst_5_modified
+import sst_2_logistic_regression
+# import sst_5_modified
 img = Image.open("code_sst_2.png")
 
 
@@ -12,22 +12,22 @@ st.header("Training our model using SVM")
 if st.button("classification report using SVM"):
     st.image(img, width = 2000)
 
-acc = sst_5_modified.print_accuracy()
-st.text("The training accuracy is:" )
-st.text(acc)
+# acc = sst_5_modified.print_accuracy()
+# st.text("The training accuracy is:" )
+# st.text(acc)
 
-# name = st.text_input("Enter ur review about bahubali", "Type Here ...")
-
-# # display the name when the submit button is clicked
-# # .title() is used to get the input text string
-# if(st.button('Submit')):
-#     out = sst_2_logistic_regression.predict_sentiment(name)
-#     st.text(out)
-
-name1 = st.text_input("Enter ur review about bahubali, we will predict using sst5 trained model", "")
+name = st.text_input("Enter ur review about bahubali", "Type Here ...")
 
 # display the name when the submit button is clicked
 # .title() is used to get the input text string
-if(st.button('PREDICT')):
-    out = sst_5_modified.predict_sentiment_sentence(name1)
+if(st.button('Submit')):
+    out = sst_2_logistic_regression.predict_sentiment(name)
     st.text(out)
+
+# name1 = st.text_input("Enter ur review about bahubali, we will predict using sst5 trained model", "")
+
+# # display the name when the submit button is clicked
+# # .title() is used to get the input text string
+# if(st.button('PREDICT')):
+#     out = sst_5_modified.predict_sentiment_sentence(name1)
+#     st.text(out)
