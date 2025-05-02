8814824ba2433b7a5a740ef3df8f4e5321f795f7
Avaneesh Sai Sammeta <23b0973@iitb.ac.in>
2025-05-02 17:53:37 +0530

    Update sst_5_modified.py

--- sst_5_modified.py
+++ sst_5_modified.py
@@ -76,11 +76,11 @@ def predict_sentiment_sentence(sentence):
         value += predict_sentiment_phrase(phrases[i])
     value /= len_phrases  # Compute average sentiment score based on phrases
 
-    # if value == 2:
-    #     out = "This review is neutral"
-    # elif value < 2:
-    #     out = "This review is negative"
-    # else:
-    #     out = "This review is positive"
+    if value < 1.5:
+        out = "This review is negative"
+    elif value < 2.5:
+        out = "This review is neutral"
+    else:
+        out = "This review is positive"
     
-    return value
+    return out
